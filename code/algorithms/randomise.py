a9fd47aa8f4d0495ed52b273f49127d8f40e6e16
bobn0172 <luckybob46@gmail.com>
2021-01-08 00:21:01 +0100

    random algorithm

---
+++ code/algorithms/randomise.py
@@ -0,0 +1,28 @@
+import random
+
+def random_moves_algorithm(board):
+
+    print(board.load_board())
+    moves = []
+    counter = 0
+
+    while not board.win():
+        possible_moves = list(board.pos_moves().items())
+
+        rand = random.choice(possible_moves)
+
+        if rand[1] != []:
+            vehicle_name = rand[0]
+            possible_shifts = rand[1]
+            shift = random.choice(possible_shifts)
+
+            board.move(vehicle_name, int(shift))
+            print(board.load_board())
+
+            moves.append([vehicle_name, int(shift)])
+            counter += 1
+
+    if board.win():
+        print('game won')
+
+    return moves
