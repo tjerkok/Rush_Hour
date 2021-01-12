bfa900f63a51129d9cb703f3c0a045711ea2434e
tjerkok <tjerko.kieft@gmail.com>
2021-01-12 15:09:22 +0100

    Added moves attribute

--- code/classes/board.py
+++ code/classes/board.py
@@ -33,6 +33,7 @@ class Board(object):
         self.boardsize = boardsize
         self.board = []
         self.possible_moves = {}
+        self.moves = []
 
     def load_board(self):
         """Loads the boards with the vehicles"""
@@ -162,6 +163,7 @@ class Board(object):
             else:
                 vehicle.coordinates = (vehicle.coordinates[0], vehicle.coordinates[1] - shift)
 
+            self.moves.append([vehicle_name, shift])
             return True
 
         return False
