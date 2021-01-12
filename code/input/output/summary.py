43766f8a72cdcfa0310a1d30263d766fcfa36415
tjerkok <tjerko.kieft@gmail.com>
2021-01-12 15:10:31 +0100

    Initial commit

---
+++ code/input/output/summary.py
@@ -0,0 +1,20 @@
+#########################################################
+# generate_output.py
+#
+# Programmeertheorie, Rush Hour
+# Tjerko Kieft, Bob Nieuwenhuize, Kika Banning
+#
+# Generates log output in a csv file
+#########################################################
+
+import csv
+
+
+def summary(filename, algorithm, amount_of_moves, states, time):
+    """Generates a log csv file with the algorithms done"""
+
+    with open('output/log.csv', 'a', newline='') as csvfile:
+        a = csv.writer(csvfile, delimiter=',')
+        row = [filename, algorithm, amount_of_moves, states, time]
+        a.writerow(row)
+    return a
