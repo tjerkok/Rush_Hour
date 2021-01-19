1b0307379eab0a79ee4d6cdfae5b670e86549740
Kika <kika_banning@hotmail.com>
2021-01-19 13:25:47 +0100

    Small changes

--- code/algorithms/priority.py
+++ code/algorithms/priority.py
@@ -6,28 +6,20 @@
 #
 # Function to apply a priority on a queue of states. This uses the
 # Heuristic that a board with the most possible moves is priority.
-# item = queue with all states
-# It returns queue with in the front the priority cues.
+# It returns a list with in the front the priority states. 
 ###############################################################
-
 
 def Priority(items):
     """A function that uses a Priority Cue search, which prioritizes the states with the most possible moves."""
 
-    # total_moves = 0
-    # count = 0
     items_tuple = []
     for item in items:
+        # Calculate total possible moves per state 
         total_moves = sum(len(moves) for moves in item.pos_moves().values())
-
         items_tuple.append([item, total_moves])
-
-    sorted_items = sorted(items_tuple, key=lambda x: x[1], reverse=True)  # sorteert de lijst op het tweede element in de tuple, dus het aantal mogelijke moves
+    
+    # Sort states per depth on most possible moves 
+    sorted_items = sorted(items_tuple, key=lambda x: x[1], reverse=True) 
     sorted_list = [item[0] for item in sorted_items]
 
     return sorted_list
-
-    # item.pos_moves().values() geeft per state per vehicle een lijst met alle possible moves
-    # eerst wordt moves uit item.pos_moves() gepakt met for moves in item.pos_moves().values(), dus moves = dict([moves van auto a], [moves van auto b])
-    # daarna wordt voor elke moves de car eruit gepakt, waardoor bijv. car = [-1, 1, 2], dus dan de som van alle lengtes van de cars
-    # dus items_tuple wordt een lijst met de state en dan het aantal moves [[state, 4], [state, 8], etc]
