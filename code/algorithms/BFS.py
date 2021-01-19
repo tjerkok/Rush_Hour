15ade28b6be57c83402a38d83945b25b387ba2ea
tjerkok <tjerko.kieft@gmail.com>
2021-01-19 16:43:36 +0100

    Changed to class

--- code/algorithms/BFS.py
+++ code/algorithms/BFS.py
@@ -9,58 +9,53 @@
 
 from .beam import Beam
 from .priority import Priority
-import queue
 import copy
+import time
 
+class BFS:
+    def __init__(self, board):
+        self.board = copy.deepcopy(board)
+        self.states = [copy.deepcopy(self.board)]
+        self.boards_visited = set()
+        self.winning_board = None
+        self.state_space = 1
 
-def BFS(board, beam, priority, heuristic='H1'):
-    """Function that plays the Rush Hour game with a Breadth First Search algorithm"""
+    def get_next_state(self):
+        """Gets the next state from the list of states."""
+        return self.states.pop(0)
 
-    BFS_queue = queue.Queue()
-    BFS_queue.put(board)
-    boards_visited = set()
-    states = 1
-    move = 0
-    apply_priority = round(2.33 * board.boardsize)
-
-    while not BFS_queue.empty():
-        # print(BFS_queue.qsize())
-        state = BFS_queue.get()
-        if move < len(state.moves):
-            print(len(state.moves))
-            if beam:
-                length = BFS_queue.qsize()
-                beamed_list = Beam(list(BFS_queue.queue), length, board.boardsize, len(board.vehicles), heuristic)
-                BFS_queue = queue.Queue()
-                [BFS_queue.put(item) for item in beamed_list]
-            if priority and move > apply_priority:
-                priority_list = Priority(list(BFS_queue.queue))
-                BFS_queue = queue.Queue()
-                [BFS_queue.put(item) for item in priority_list]
-            move = len(state.moves)
-
-        for vehicle, movelist in state.pos_moves().items():
+    def build_children(self, board):
+        """Creates all possible child-states."""
+        for vehicle, movelist in board.pos_moves().items():
             for vehicle_move in movelist:
-
-                if not state.move(vehicle, vehicle_move):
+                child = copy.deepcopy(board)
+                if not child.move(vehicle, vehicle_move):
                     print("invalid move")
+                    return False
+                self.add_to_archive(child)
+                self.state_space += 1
 
-                else:
-                    state_board = state.load_board()
+    def add_to_archive(self, board):
+        """Function that adds the checked states to the archive."""
+        board_array = board.load_board()
+        board_array.flags.writeable = False 
+        hashed_board = hash(board_array.tostring())
+        if hashed_board not in self.boards_visited:
+            self.boards_visited.add(hashed_board)
+            self.states.append(board)
 
-                    state_board.flags.writeable = False
-                    hashed_state = hash(state_board.tostring())
-                    if hashed_state not in boards_visited:
-                        boards_visited.add(hashed_state)
-                        BFS_queue.put(copy.deepcopy(state))
-                        states += 1
+    def run(self):  
+        """Runs the algorithm untill all possible states are checked."""
+        # TO DO Beam en priority toevoegen 
 
-                    if state.win():
-                        BFS_queue = queue.Queue()
-                        winning_board = state
-                    else:
-                        state.move(vehicle, -vehicle_move, True)
-                        state.load_board()
-                        state.pos_moves()
+        while self.states != []: 
+            new_board = self.get_next_state()
 
-    return winning_board, states
+            if new_board.win():
+                self.winning_board = new_board
+                return self.winning_board, self.state_space
+            else: 
+                self.build_children(new_board) # lijst vullen met kids
+
+        # if queue empty but not won, return None board
+        return None, self.state_space
