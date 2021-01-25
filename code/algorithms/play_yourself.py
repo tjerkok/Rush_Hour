934879f547774b8597a215263a0dd62a36d0d2f2
bobn0172 <luckybob46@gmail.com>
2021-01-25 17:27:52 +0100

    play yourself updated

--- code/algorithms/play_yourself.py
+++ code/algorithms/play_yourself.py
@@ -16,23 +16,24 @@ def play(board):
 
     while not board.win() and counter < max_moves:
 
-        # User input for move
-        vehicle_name = input('letter of the vehicle you want to move: ').upper()
-        shift = input('places to move that vehicle, use minus for left/up: ')
         board.pos_moves()
 
+        # User input for move
+        vehicle_name = input("letter of the vehicle you want to move: ")
+        shift = input("places to move that vehicle, use minus for left/up: ")
+
         # Vehicle moves if possible
-        if board.move(vehicle_name, int(shift)):
+        if board.move(vehicle_name.upper(), int(shift)):
             print(board.load_board())
             counter += 1
         else:
-            print('not allowed')
+            print("not allowed")
 
     if board.win():
-        print('game won')
+        print("game won")
         winning_board = board
 
     elif counter == max_moves:
-        print('maximum moves reached')
+        print("maximum moves reached")
 
     return winning_board
