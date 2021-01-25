10f0782229e51ac083ef725a58c3cef1d9dff9cc
Kika <kika_banning@hotmail.com>
2021-01-25 22:00:02 +0100

    cleaned-up randomise

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -5,13 +5,14 @@
 # Tjerko Kieft, Bob Nieuwenhuize, Kika Banning
 #
 # Function with randomise algorithm to play the Rush Hour game.
+# Receives sample size input.
+# Returns lowest_moves_board, amount_of_moves, time1
 ###############################################################
 
 import random
 import time
 import csv
 from ..input.output.load_in import load_problem
-
 
 def random_moves_algorithm(filename, sample_size):
     """Function that plays the Rush Hour game with a randomise algorithm."""
@@ -26,34 +27,32 @@ def random_moves_algorithm(filename, sample_size):
         board.load_board()
 
         while not board.win():
-            # Lists all possible moves and picks one random
+
+            # lists all possible moves and picks random vehicle
             possible_moves = list(board.pos_moves().items())
             rand = random.choice(possible_moves)
 
-            # Moves vehicle if possible, else picks new random move
+            # picks random move for vehicle
             if rand[1] != []:
                 vehicle_name = rand[0]
                 possible_shifts = rand[1]
                 shift = random.choice(possible_shifts)
-
+                 
+                # moves vehicle
                 board.move(vehicle_name, int(shift))
                 board.load_board()
 
         if board.win():
             winning_board = board
-            # print(winning_board.load_board())
 
+            # keeps track of minimal amount of moves 
             if len(winning_board.moves) < min_moves:
                 lowest_moves_board = winning_board
                 min_moves = len(winning_board.moves)
 
+        # keeps track of time, moves and boards
         time1.append(time.time() - time0)
         amount_of_moves.append(len(winning_board.moves))
         print(f"boards won: {i + 1}")
-
-    with open('output/baseline.csv', 'a', newline='') as csvfile:
-        a = csv.writer(csvfile, delimiter=',')
-        a.writerow([filename, amount_of_moves])
-        a.writerows(map(lambda x: [x], amount_of_moves))
 
     return lowest_moves_board, amount_of_moves, time1
