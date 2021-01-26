c6f38f497a6ef9ed6a82ac7979ce7fd9bf6111a7
Kika <kika_banning@hotmail.com>
2021-01-26 12:00:04 +0100

    changed folder

---
+++ code/input_output/generate_output.py
@@ -0,0 +1,21 @@
+#######################################################
+# generate_output.py
+#
+# Programmeertheorie, Rush Hour
+# Tjerko Kieft, Bob Nieuwenhuize, Kika Banning
+#
+# Generates output in a csv file: output/output.csv
+# the output includes all performed moves. 
+#######################################################
+
+import csv
+
+def output(moves):
+    """Generates an output csv file with all moves per car"""
+
+    with open('output/output.csv', 'w', newline='') as csvfile:
+        output = csv.writer(csvfile, delimiter=',')
+        output.writerow(['car', 'move'])
+        for move in moves:
+            output.writerow(move)
+    return output
---
+++ code/input_output/summary.py
@@ -0,0 +1,18 @@
+#########################################################
+# generate_output.py
+#
+# Programmeertheorie, Rush Hour
+# Tjerko Kieft, Bob Nieuwenhuize, Kika Banning
+#
+# Generates log output in a csv file
+#########################################################
+
+import csv
+
+def summary(filename, algorithm, amount_of_moves, states, time):
+    """Generates a log csv file with the algorithms done"""
+
+    with open('output/log.csv', 'a', newline='') as csvfile:
+        newline = csv.writer(csvfile, delimiter=',')
+        row = [filename, algorithm, amount_of_moves, states, time]
+        newline.writerow(row)
